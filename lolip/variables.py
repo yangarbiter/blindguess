6253f18faa9c15b19c5dc024e4d6ddf6923eaa5d
yangarbiter <yangarbiter@gmail.com>
2019-12-23 11:12:37 -0800

    update

--- lolip/variables.py
+++ lolip/variables.py
@@ -4,6 +4,7 @@ from functools import partial
 from autovar import AutoVar
 from autovar.base import RegisteringChoiceType, register_var, VariableClass
 from autovar.hooks import check_result_file_exist, save_result_to_file
+from autovar.hooks import create_placeholder_file
 from autovar.hooks import default_get_file_name as get_file_name
 
 from .datasets import DatasetVarClass
@@ -12,7 +13,10 @@ from .attacks import AttackVarClass
 
 auto_var = AutoVar(
     logging_level=logging.INFO,
-    before_experiment_hooks=[partial(check_result_file_exist, get_name_fn=get_file_name)],
+    before_experiment_hooks=[
+        partial(check_result_file_exist, get_name_fn=get_file_name)
+        partial(create_placeholder_file, get_name_fn=get_file_name)
+    ],
     after_experiment_hooks=[partial(save_result_to_file, get_name_fn=get_file_name)],
     settings={
         'file_format': 'pickle',
