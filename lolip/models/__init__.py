e84b1a3b6a49e38f1d3fcab559fff87663f0ebe1
yangarbiter <yangarbiter@gmail.com>
2019-12-28 14:33:10 -0800

    tune hyper param

--- lolip/models/__init__.py
+++ lolip/models/__init__.py
@@ -22,6 +22,8 @@ def get_hyper(name, loss, arch, dataset_name):
         if 'fashion' in dataset_name and 'adv' in loss:
             ret['learning_rate'] = 1e-3
         elif 'cifar' in dataset_name and 'cure' in loss:
+            ret['learning_rate'] = 1e-5
+        elif 'llr' in loss:
             ret['learning_rate'] = 1e-3
         else:
             ret['learning_rate'] = 1e-2
