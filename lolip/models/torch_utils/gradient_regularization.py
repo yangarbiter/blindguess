4f1de537226bc3160a8d791d8680763b1de903e7
yangarbiter <yangarbiter@gmail.com>
2019-12-27 16:17:19 -0800

    add gradient_regularization

---
+++ lolip/models/torch_utils/gradient_regularization.py
@@ -0,0 +1,26 @@
+"""
+Improving the Adversarial Robustness and Interpretability of Deep Neural Networks by Regularizing their Input Gradients
+"""
+
+import torch
+
+def gradient_regularization(model, loss_fn, optimizer, x, y, lambd):
+
+    def model_grad(x):
+        x.requires_grad_(True)
+        lx = loss_fn(model(x), y)
+        lx.backward()
+        ret = x.grad
+        x.grad.zero_()
+        x.requires_grad_(False)
+        return ret
+
+    regularization = lambd * torch.norm(model_grad(x), p=2)**2
+
+    optimizer.zero_grad()
+    outputs = model(x)
+    loss_natural = loss_fn(outputs, y)
+
+    loss = loss_natural + lambd * gradient_regularization
+
+    return outputs, loss
