fix lip KL

lolip/utils.py
@@ -11,11 +11,10 @@ def local_lip(model, x, xp, top_norm, btm_norm, reduction='mean'):
     model.eval()
     down = torch.flatten(x - xp, start_dim=1)
     if top_norm == "kl":
-        criterion_kl = nn.KLDivLoss(reduction=None)
+        criterion_kl = nn.KLDivLoss(reduction='none')
         top = criterion_kl(F.log_softmax(model(x), dim=1),
                            F.softmax(model(xp), dim=1))
-        #top = torch.flatten(top, start_dim=1)
-        ret = top / torch.norm(down + 1e-6, dim=1, p=btm_norm)
+        ret = torch.sum(top, dim=1) / torch.norm(down + 1e-6, dim=1, p=btm_norm)
     else:
         top = torch.flatten(model(x), start_dim=1) - torch.flatten(model(xp), start_dim=1)
         ret = torch.norm(top, dim=1, p=top_norm) / torch.norm(down + 1e-6, dim=1, p=btm_norm)