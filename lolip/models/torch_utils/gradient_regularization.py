e1a6feb69e56c622c4398162907d72511916dff6
yangarbiter <yangarbiter@gmail.com>
2020-01-12 00:36:30 -0800

    update

--- lolip/models/torch_utils/gradient_regularization.py
+++ lolip/models/torch_utils/gradient_regularization.py
@@ -7,23 +7,17 @@ from torch.autograd import grad
 
 
 def gradient_regularization(model, loss_fn, optimizer, x, y, lambd):
-
-
-    def model_grad(x):
-        x.requires_grad_(True)
-        ret = grad(loss_fn, x, retain_graph=True)[0]
-        x.requires_grad_(False)
-        return ret
-
     optimizer.zero_grad()
+
     x.requires_grad_(True)
     outputs = model(x)
     lx = loss_fn(outputs, y)
-    x_grad = grad(lx, x, retain_graph=True)[0]
-    regularization = torch.norm(x_grad.flatten(start_dim=1), dim=1, p=2)**2
+    x_grad = grad(lx, x, retain_graph=True, create_graph=True)[0]
+    #x.requires_grad_(False)
+
+    regularization = x_grad**2
     regularization = torch.sum(regularization)
     #x.grad.zero_()
-    x.requires_grad_(False)
 
     #outputs = model(x)
     #loss_natural = loss_fn(outputs, y)
