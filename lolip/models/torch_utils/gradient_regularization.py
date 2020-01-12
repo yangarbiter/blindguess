ef308a8d0033a76d6f2201e639143030c82d607f
yangarbiter <yangarbiter@gmail.com>
2020-01-11 17:18:48 -0800

    update

--- lolip/models/torch_utils/gradient_regularization.py
+++ lolip/models/torch_utils/gradient_regularization.py
@@ -15,10 +15,10 @@ def gradient_regularization(model, loss_fn, optimizer, x, y, lambd):
         x.requires_grad_(False)
         return ret
 
+    optimizer.zero_grad()
     regularization = torch.norm(model_grad(x).flatten(start_dim=1), dim=1, p=2)**2
     regularization = torch.sum(regularization)
 
-    optimizer.zero_grad()
     outputs = model(x)
     loss_natural = loss_fn(outputs, y)
 
