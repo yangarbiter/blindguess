2492770cce0805246b1e1a2427c06b18a9d06714
yangarbiter <yangarbiter@gmail.com>
2020-01-11 22:41:48 -0800

    update

--- lolip/models/torch_utils/gradient_regularization.py
+++ lolip/models/torch_utils/gradient_regularization.py
@@ -3,21 +3,27 @@ Improving the Adversarial Robustness and Interpretability of Deep Neural Network
 """
 
 import torch
+from torch.autograd import grad
+
 
 def gradient_regularization(model, loss_fn, optimizer, x, y, lambd):
 
+
     def model_grad(x):
         x.requires_grad_(True)
-        lx = loss_fn(model(x), y)
-        lx.backward()
-        ret = x.grad
-        #x.grad.zero_()
+        ret = grad(loss_fn, x, retain_graph=True)[0]
         x.requires_grad_(False)
         return ret
 
     optimizer.zero_grad()
-    regularization = torch.norm(model_grad(x).flatten(start_dim=1), dim=1, p=2)**2
+    x.requires_grad_(True)
+    lx = loss_fn(model(x), y)
+    x_grad = grad(loss_fn, x, retain_graph=True)[0]
+    lx.backward()
+    regularization = torch.norm(x_grad.flatten(start_dim=1), dim=1, p=2)**2
     regularization = torch.sum(regularization)
+    #x.grad.zero_()
+    x.requires_grad_(False)
 
     outputs = model(x)
     loss_natural = loss_fn(outputs, y)
