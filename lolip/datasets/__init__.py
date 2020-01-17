c018a66ac25e8a26c3ec95b34742e2ec01eb2262
yangarbiter <yangarbiter@gmail.com>
2020-01-16 17:57:10 -0800

    update resImgnet dataset

--- lolip/datasets/__init__.py
+++ lolip/datasets/__init__.py
@@ -205,15 +205,15 @@ class DatasetVarClass(VariableClass, metaclass=RegisteringChoiceType):
     @register_var(argument=r"resImgnet", shown_name="Restricted ImageNet")
     @staticmethod
     def resImgnet(auto_var, inter_var):
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+        #normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        #                             std=[0.229, 0.224, 0.225])
         trn_ds = ImageFolder(
             "/tmp2/RestrictedImgNet/train",
             transform=transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                normalize,
+                #normalize,
             ]))
         tst_ds = ImageFolder(
             "/tmp2/RestrictedImgNet/val",
@@ -221,7 +221,7 @@ class DatasetVarClass(VariableClass, metaclass=RegisteringChoiceType):
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
-                normalize,
+                #normalize,
             ]))
         return trn_ds, tst_ds
 
